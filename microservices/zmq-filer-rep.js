c6f2fa0c6d7817263cc7c15f9fce4ab42e858087
Michael Keyes <keyeswmike@gmail.com>
2019-01-17 10:57:25 -0500

    zmq responder

---
+++ microservices/zmq-filer-rep.js
@@ -0,0 +1,34 @@
+'use scrict';
+const fs = require('fs');
+const zmq = require('zeromq');
+
+const responder = zmq.socket('rep');
+
+responder.on('message', (data) => {
+    const request = JSON.parse(data);
+    console.log(`Recived a requires to get: ${request.path}`);
+
+    // Read the file and reply with the content
+    fs.readFile(request.path, (err, content) => {
+        console.log('Sending response content');
+        responder.send(JSON.stringify({
+            content: content.toString(),
+            timestamp: Date.now(),
+            pid: process.pid
+        }));
+    });
+});
+
+// Listen on TCP port 60401
+responder.bind('tcp://127.0.0.1:60401', (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Listening for zmq requesters...');
+});
+
+// Close the responder when the Node process ends.
+process.on('SIGINT', () => {
+    console.log('Shutting down...');
+    responder.close();
+});

fdc2f24fa0a7341070ecf9acb63c3c5a8b516701
Michael Keyes <keyeswmike@gmail.com>
2019-01-19 11:11:00 -0500

    declaring expectations with chai

---
+++ datawork/databases/test/parse-rdf-test.js
@@ -0,0 +1,11 @@
+'use scrict';
+
+const fs = require('fs');
+const expect = require('chai').expect;
+const rdf = fs.readFileSync(`${__dirname}/pg132.rdf`);
+
+describe('parseRDF', () => {
+    it('should be a function', () => {
+        expect(parseRDF).to.be.a('function');
+    });
+});

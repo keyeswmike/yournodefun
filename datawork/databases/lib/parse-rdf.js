e5692be13c7708b6e50dda3a1ae17a8792a01e2d
Michael Keyes <keyeswmike@gmail.com>
2019-01-20 09:04:49 -0500

    start parsing xml with Cheerio

--- datawork/databases/lib/parse-rdf.js
+++ datawork/databases/lib/parse-rdf.js
@@ -1,6 +1,11 @@
 'use scrict';
+const cheerio = require('cheerio');
 
 module.exports = (rdf) => {
+    const $ = cheerio.load(rdf);
+
     const book = {};
+    book.id = +$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', '');
+    book.title = $('dcterms\\:title').text();
     return book;
 };
--- datawork/databases/test/parse-rdf-test.js
+++ datawork/databases/test/parse-rdf-test.js
@@ -10,8 +10,11 @@ describe('parseRDF', () => {
         expect(parseRDF).to.be.a('function');
     });
 
-    it('should return an object', () => {
+    it('should parse RDF content', () => {
         const book = parseRDF(rdf);
         expect(book).to.be.an('object');
+        expect(book).to.have.a.property('id', 132);
+        expect(book).to.have.a.property('title', 'The Art of War');
     });
+
 });

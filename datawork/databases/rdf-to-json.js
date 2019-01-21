2c9dc8af247ce34937d6df0f937f5d88c86d787a
Michael Keyes <keyeswmike@gmail.com>
2019-01-21 09:43:48 -0500

    convert single rdf to json object

---
+++ datawork/databases/rdf-to-json.js
@@ -0,0 +1,6 @@
+'use scrict';
+const fs = require('fs');
+const parseRDF = require('./lib/parse-rdf.js');
+const rdf = fs.readFileSync(process.argv[2]);
+const book = parseRDF(rdf);
+console.log(JSON.stringify(book, null, '   '));
